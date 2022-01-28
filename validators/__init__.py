b74692deb8857406675a4a627cc3905c6cd7bba1
JesusRL96 <jesusalbertoramirezlopez@gmail.com>
2022-01-28 11:06:23 -0600

    create query validator class

--- validators/__init__.py
+++ validators/__init__.py
@@ -1,6 +1,14 @@
 from .validators import (
-    ValidateChoices,
     ValidatorBase,
+    ValidateChoices,
     ValidatorRegExp,
     ValidateIsClose
 )
+
+
+class QueryValidators:
+
+    validators = [ValidateChoices, ValidateIsClose, ValidatorRegExp,]
+
+    def get_validator_types(self):
+        return [x.__name__ for x in self.validators]
